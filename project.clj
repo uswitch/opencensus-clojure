d72c47b7de388801816109864a2d54b6ec9903d5
Emils Solmanis <emils.solmanis@uswitch.com>
2018-05-01 11:50:53 +0100

    bump to 0.13

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :description "nope"
   :url "http://example.com/fridayfriday"
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [io.opencensus/opencensus-api "0.12.3"]
-                 [io.opencensus/opencensus-exporter-trace-logging "0.12.3"]
-                 [io.opencensus/opencensus-impl "0.12.3" :scope "runtime"]
-                 [io.opencensus/opencensus-exporter-trace-jaeger "0.13.0-SNAPSHOT"]]
+                 [io.opencensus/opencensus-api "0.13.0"]
+                 [io.opencensus/opencensus-exporter-trace-logging "0.13.0"]
+                 [io.opencensus/opencensus-impl "0.13.0" :scope "runtime"]
+                 [io.opencensus/opencensus-exporter-trace-jaeger "0.13.0"]]
 
   :repositories {"releases" {:url "http://internaljars.uswitchinternal.com:8080/nexus/content/repositories/releases"
                              :sign-releases false
